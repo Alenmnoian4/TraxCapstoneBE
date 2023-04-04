41ab4b34156a2a1611d7593822b78ad63309c968
Alenmnoian4 <alenmnoian4@gmail.com>
2023-04-04 02:45:11 -0400

    commit

--- traxs/serializers.py
+++ traxs/serializers.py
@@ -2,8 +2,8 @@ from .models import Trax
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-# Our TodoSerializer
-class TodoSerializer(serializers.HyperlinkedModelSerializer):
+# Our TraxSerializer
+class TraxSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         # The model it will serialize
         model = Trax
